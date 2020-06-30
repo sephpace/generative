9998f52130ed5a1e263602a564e24025608e4e5c
seph <sephpace@gmail.com>
2020-06-30 12:58:23 -0600

    Create AutoEncoder model.

--- models.py
+++ models.py
@@ -3,6 +3,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class AutoEncoder(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(AutoEncoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, x):
+        z, indices = self.encoder(x)
+        y = self.decoder(z, indices)
+        return y
+
+
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
